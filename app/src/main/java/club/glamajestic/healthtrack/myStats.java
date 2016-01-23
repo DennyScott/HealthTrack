d1d63a077084f89ad13289e17cb8cdb20aeb4f48
elsalakk <elsalakk@gmail.com>
2016-01-23 00:15:58 -0600

    Added a library, and modified Gradel accordingly

---
+++ app/src/main/java/club/glamajestic/healthtrack/myStats.java
@@ -0,0 +1,28 @@
+package club.glamajestic.healthtrack;
+
+/**
+ * Created by Khaled on 1/22/2016.
+**/
+import java.util.ArrayList;
+
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.components.Legend.LegendPosition;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.listener.OnChartValueSelectedListener;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
+import android.app.Activity;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
+
+public class myStats {
+
+
+}
