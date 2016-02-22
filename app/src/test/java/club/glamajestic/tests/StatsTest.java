2fa691cbbe88c420a044ccac99212298f21136bc
Scott Woodcock <umwoodcs@myumanitoba.ca>
2016-02-21 21:27:34 -0600

    jUnit tests completed jUnit tests written to separate each test into a method, and init a new statsBus each method. Removed test annotation on class extending TestCase (Warnings)

--- app/src/test/java/club/glamajestic/tests/StatsTest.java
+++ app/src/test/java/club/glamajestic/tests/StatsTest.java
@@ -5,36 +5,31 @@ import org.junit.Test;
 
 import business.Stats;
 
-
-
-import org.junit.Test;
-
 public class StatsTest extends TestCase{
     String[] keys, otherKeys;
     float[] values, otherValues;
 
-    @Test
     public void testNullKeys(){
         Stats statsBus = new Stats();
         statsBus.init(1);
         keys = statsBus.getKeys();
         assertTrue("Keys are null", keys!=null);
     }
-    @Test
+
     public void testNullOtherKeys(){
         Stats statsBus = new Stats();
         statsBus.init(1);
         otherKeys = statsBus.getOtherKeys();
         assertTrue("Other Keys are null", otherKeys!=null);
     }
-    @Test
+
     public void testNullValues(){
         Stats statsBus = new Stats();
         statsBus.init(1);
         values = statsBus.getValues();
         assertTrue("Values are null", values!=null);
     }
-    @Test
+
     public void testNullOtherValues(){
         Stats statsBus = new Stats();
         statsBus.init(1);
@@ -42,7 +37,6 @@ public class StatsTest extends TestCase{
         assertTrue("Other Values are null", otherValues!=null);
     }
 
-    @Test
     public void testLength(){
         Stats statsBus = new Stats();
         statsBus.init(1);
@@ -53,7 +47,6 @@ public class StatsTest extends TestCase{
         assertTrue("Keys have zero or negative length", keys.length > 0 && values.length > 0 && otherKeys.length > 0 && otherValues.length > 0);
     }
 
-    @Test
     public void testEqualLength1(){
         Stats statsBus = new Stats();
         statsBus.init(1);
