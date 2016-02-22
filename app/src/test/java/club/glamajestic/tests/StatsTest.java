ad16fce11a66383f6091bd1b84d08401e706b330
Scott Woodcock <umwoodcs@myumanitoba.ca>
2016-02-21 20:58:31 -0600

    jUnit tests completed jUnit tests written to separate each test into a method, and init a new statsBus each method.

--- app/src/test/java/club/glamajestic/tests/StatsTest.java
+++ app/src/test/java/club/glamajestic/tests/StatsTest.java
@@ -14,42 +14,53 @@ public class StatsTest extends TestCase{
     float[] values, otherValues;
 
     @Test
-    public void testNull(){
+    public void testNullKeys(){
         Stats statsBus = new Stats();
-        assertTrue("Keys are null", keys!=null && values!=null || otherKeys!=null || otherValues!=null);
+        statsBus.init(1);
+        keys = statsBus.getKeys();
+        assertTrue("Keys are null", keys!=null);
     }
     @Test
-    public void testWtf(){
+    public void testNullOtherKeys(){
         Stats statsBus = new Stats();
-        assertTrue(false);
+        statsBus.init(1);
+        otherKeys = statsBus.getOtherKeys();
+        assertTrue("Other Keys are null", otherKeys!=null);
+    }
+    @Test
+    public void testNullValues(){
+        Stats statsBus = new Stats();
+        statsBus.init(1);
+        values = statsBus.getValues();
+        assertTrue("Values are null", values!=null);
+    }
+    @Test
+    public void testNullOtherValues(){
+        Stats statsBus = new Stats();
+        statsBus.init(1);
+        otherValues = statsBus.getOtherValues();
+        assertTrue("Other Values are null", otherValues!=null);
     }
 
-
+    @Test
     public void testLength(){
         Stats statsBus = new Stats();
+        statsBus.init(1);
+        keys=statsBus.getKeys();
+        otherKeys=statsBus.getOtherKeys();
+        values=statsBus.getValues();
+        otherValues=statsBus.getOtherValues();
         assertTrue("Keys have zero or negative length", keys.length > 0 && values.length > 0 && otherKeys.length > 0 && otherValues.length > 0);
     }
 
-    public void testEqualLength(){
-        Stats statsBus = new Stats();
-        assertTrue("Keys are not equal length", keys.length == values.length && otherKeys.length == otherValues.length);
-    }
-
-    public void testNull1(){
-        Stats statsBus = new Stats();
-        statsBus.init(1);
-        assertTrue("Keys are null", keys!=null && values!=null || otherKeys!=null || otherValues!=null);
-    }
-
-    public void testLength1(){
-        Stats statsBus = new Stats();
-        statsBus.init(1);
-        assertTrue("Keys have zero or negative length", keys.length > 0 && values.length > 0 && otherKeys.length > 0 && otherValues.length > 0);
-    }
-
+    @Test
     public void testEqualLength1(){
         Stats statsBus = new Stats();
         statsBus.init(1);
+        keys=statsBus.getKeys();
+        otherKeys=statsBus.getOtherKeys();
+        values=statsBus.getValues();
+        otherValues=statsBus.getOtherValues();
         assertTrue("Keys are not equal length", keys.length == values.length && otherKeys.length == otherValues.length);
     }
 
