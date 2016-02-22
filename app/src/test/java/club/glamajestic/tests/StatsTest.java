8e2d7cb3ac63f6f9e0930e1c7dbcc2b6571a0d86
Scott Woodcock <umwoodcs@myumanitoba.ca>
2016-02-21 18:48:02 -0600

    Wilson rewrote unit tests to use jUnit assertTrue statements

--- app/src/test/java/club/glamajestic/tests/StatsTest.java
+++ app/src/test/java/club/glamajestic/tests/StatsTest.java
@@ -1,5 +1,7 @@
 package club.glamajestic.tests;
 import junit.framework.TestCase;
+import org.junit.Test;
+
 
 import business.Stats;
 
@@ -12,58 +14,43 @@ public class StatsTest extends TestCase{
     float[] values, otherValues;
 
     @Test
-    public void test() throws Exception{
-        boolean passed = true;
-        //////////////////////////////////////////////////////////////////////////////////testing use of public methods.
-        Stats statsBus= new Stats();
-        for(int x = 1; x <3; x++) {
-            try {
-                keys = statsBus.getKeys();
-            } catch (Exception e) {
-                System.out.println("failed to get keys: " + e.getMessage());
-                passed = false;
-                break;
-            }
-            try {
-                otherKeys = statsBus.getOtherKeys();
-            } catch (Exception e) {
-                System.out.println("failed to get keys: " + e.getMessage());
-                passed = false;
-                break;
-            }
-            try {
-                values = statsBus.getValues();
-            } catch (Exception e) {
-                System.out.println("failed to get keys: " + e.getMessage());
-                passed = false;
-                break;
-            }
-            try {
-                otherValues = statsBus.getOtherValues();
-            } catch (Exception e) {
-                System.out.println("failed to get keys: " + e.getMessage());
-                passed = false;
-                break;
-            }
-            //////////////////////////////////////////////////////////////////////////////////Making sure non of them return null
-            if (keys == null || values == null && otherKeys == null && otherValues == null) {
-                passed = false;
-                break;
-            }
-            //////////////////////////////////////////////////////////////////////////////////Making sure they all have a size of at least 1
-            if (keys.length <= 0 || values.length <= 0 || otherKeys.length <= 0 || otherValues.length <= 0) {
-                passed = false;
-                break;
-            }
-            //////////////////////////////////////////////////////////////////////////////////Making sure they are equivelent to their counter parts
-            if (keys.length != values.length || otherKeys.length != otherValues.length) {
-                passed = false;
-                break;
-            }
-            statsBus.init(x);// do test for all modes
-        }
+    public void testNull(){
+        Stats statsBus = new Stats();
+        assertTrue("Keys are null", keys!=null && values!=null || otherKeys!=null || otherValues!=null);
+    }
+    @Test
+    public void testWtf(){
+        Stats statsBus = new Stats();
+        assertTrue(false);
+    }
 
-        assertTrue(passed);
+
+    public void testLength(){
+        Stats statsBus = new Stats();
+        assertTrue("Keys have zero or negative length", keys.length > 0 && values.length > 0 && otherKeys.length > 0 && otherValues.length > 0);
+    }
+
+    public void testEqualLength(){
+        Stats statsBus = new Stats();
+        assertTrue("Keys are not equal length", keys.length == values.length && otherKeys.length == otherValues.length);
+    }
+
+    public void testNull1(){
+        Stats statsBus = new Stats();
+        statsBus.init(1);
+        assertTrue("Keys are null", keys!=null && values!=null || otherKeys!=null || otherValues!=null);
+    }
+
+    public void testLength1(){
+        Stats statsBus = new Stats();
+        statsBus.init(1);
+        assertTrue("Keys have zero or negative length", keys.length > 0 && values.length > 0 && otherKeys.length > 0 && otherValues.length > 0);
+    }
+
+    public void testEqualLength1(){
+        Stats statsBus = new Stats();
+        statsBus.init(1);
+        assertTrue("Keys are not equal length", keys.length == values.length && otherKeys.length == otherValues.length);
     }
 
 }
