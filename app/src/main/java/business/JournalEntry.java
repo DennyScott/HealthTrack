b3dfd612c986cc4e41b55a3ed9c7e20c2580b3ce
Scott Woodcock <umwoodcs@myumanitoba.ca>
2016-02-28 18:59:42 -0600

    Add business object for journal entries
    Started new class for the business object representing a journal entry including date, food eaten, and amount eaten (servings)

---
+++ app/src/main/java/business/JournalEntry.java
@@ -0,0 +1,13 @@
+package business;
+
+import java.sql.Date;
+
+/**
+ * Created by scott on 28/02/16.
+ * Represents a single entry in the food journal
+ */
+public class JournalEntry {
+    Date dateEntered;
+    //Food eaten;
+    //Serving size;
+}
