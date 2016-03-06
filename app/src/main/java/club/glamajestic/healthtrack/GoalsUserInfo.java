c87070d82d548a6999efe3100ebc599b640aa332
Wilson <w-tieu@hotmail.com>
2016-03-05 23:22:23 -0600

    User info submission page now saves user input to internal storage

--- app/src/main/java/club/glamajestic/healthtrack/GoalsUserInfo.java
+++ app/src/main/java/club/glamajestic/healthtrack/GoalsUserInfo.java
@@ -1,11 +1,24 @@
 package club.glamajestic.healthtrack;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 /**
  * <code>GoalsUserInfo</code> allows the user to enter information about themselves, which the
@@ -14,6 +27,8 @@ import android.widget.Toast;
 public class GoalsUserInfo extends Activity implements View.OnClickListener {
 
     private Button saveButton;
+    private EditText editText;
+    private TextView textView;
 
     /**
      * {@inheritDoc}
@@ -24,6 +39,9 @@ public class GoalsUserInfo extends Activity implements View.OnClickListener {
         setContentView(R.layout.goals_user_info);
         saveButton = (Button)findViewById(R.id.saveButton);
         saveButton.setOnClickListener(this);
+        editText = (EditText)findViewById(R.id.nameTextEntry);
+        textView = (TextView)findViewById(R.id.textView);
+        textView.setVisibility(View.GONE);
     }
 
     /**
@@ -59,5 +77,70 @@ public class GoalsUserInfo extends Activity implements View.OnClickListener {
 
     private void saveClicked() {
         Output.toastMessage(this, "Changes updated.", Output.SHORT_TOAST);
+        saveText();
+    }
+
+    private void saveText(){
+        /*try {
+
+            OutputStreamWriter out = new OutputStreamWriter(openFileOutput("userInfo", MODE_APPEND));
+            EditText ET = (EditText)findViewById(R.id.nameTextEntry);
+            String text = ET.getText().toString();
+            out.write(text);
+            out.write('\n');
+            out.close();
+            Toast.makeText(this, text, Toast.LENGTH_LONG).show();
+
+        } catch (Throwable t) {
+
+            Toast.makeText(this, "Exception: " + t.toString(), Toast.LENGTH_LONG).show();
+
+        }*/
+        String message = editText.getText().toString();
+        String fileName = "userInput";
+        try {
+            FileOutputStream fileOutputStream = openFileOutput(fileName,MODE_PRIVATE);
+            fileOutputStream.write(message.getBytes());
+            editText.setText("");
+
+            editText = (EditText)findViewById(R.id.ageTextEntry);
+            message = ", " + editText.getText().toString();
+            fileOutputStream.write(message.getBytes());
+            editText.setText("");
+
+            editText = (EditText)findViewById(R.id.weightTextEntry);
+            message = ", " + editText.getText().toString();
+            fileOutputStream.write(message.getBytes());
+
+            fileOutputStream.close();
+            editText.setText("");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /*
+    This is only here as a reminder of how to retrieve the user's input from internal storage.
+    Requires plain text view widget and a load button.
+     */
+    public void readMessage(View view) {
+        try {
+            String message;
+            FileInputStream fileInputStream = openFileInput("userInput");
+            InputStreamReader inputStreamReader = new InputStreamReader(fileInputStream);
+            BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+            StringBuffer stringBuffer = new StringBuffer();
+            while ((message=bufferedReader.readLine())!=null) {
+                stringBuffer.append(message + "\n");
+            }
+            textView.setText(stringBuffer.toString());
+            textView.setVisibility(View.VISIBLE);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
