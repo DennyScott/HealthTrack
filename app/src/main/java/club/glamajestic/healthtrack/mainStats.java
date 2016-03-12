9995e7d77165ab7778e8070d5db66e63cd8dedcf
elsalakk <elsalakk@gmail.com>
2016-03-11 23:22:04 -0600

    - Added Button sounds, (needs to be implemented in all classes) already implemented in stats class.
    Simply Instantiate :
    ---ClickSound playSound;
    Load on onCreate()
    ----playSound = new ClickSound(this);
    And put this in each onClick(){
    -----playSound.play();
    }

--- app/src/main/java/club/glamajestic/healthtrack/mainStats.java
+++ app/src/main/java/club/glamajestic/healthtrack/mainStats.java
@@ -17,12 +17,14 @@ import android.view.MenuItem;
 import android.widget.FrameLayout;
 import com.github.mikephil.charting.charts.PieChart;
 
+import business.ClickSound;
 import business.InitPieChart;
 
 public class mainStats extends AppCompatActivity  implements NavigationView.OnNavigationItemSelectedListener {
     private FrameLayout stats;
     private PieChart chart;
     private MediaPlayer backgroundMusic;
+    ClickSound playSound;
     boolean soundEnabled;
     int charInitMode;
 
@@ -30,6 +32,7 @@ public class mainStats extends AppCompatActivity  implements NavigationView.OnNa
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_stats);
+        playSound = new ClickSound(this);
         boolean soundEnabled = true;
         if(soundEnabled) {
             backgroundMusic = MediaPlayer.create(this, R.raw.delta);
@@ -47,6 +50,7 @@ public class mainStats extends AppCompatActivity  implements NavigationView.OnNa
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                playSound.play();
                 Snackbar.make(view, "Add Food Item", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
@@ -63,6 +67,7 @@ public class mainStats extends AppCompatActivity  implements NavigationView.OnNa
     }
     @Override
     public void onBackPressed() {
+        playSound.play();
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         if (drawer.isDrawerOpen(GravityCompat.START)) {
             drawer.closeDrawer(GravityCompat.START);
@@ -71,6 +76,13 @@ public class mainStats extends AppCompatActivity  implements NavigationView.OnNa
         }
     }
 
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (this.isFinishing()){
+            backgroundMusic.stop();
+        }
+    }
     @Override
     protected void onDestroy() {
         super.onDestroy();
@@ -93,6 +105,7 @@ public class mainStats extends AppCompatActivity  implements NavigationView.OnNa
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
+        playSound.play();
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main_stats, menu);
         return true;
@@ -100,6 +113,7 @@ public class mainStats extends AppCompatActivity  implements NavigationView.OnNa
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+        playSound.play();
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
@@ -116,6 +130,7 @@ public class mainStats extends AppCompatActivity  implements NavigationView.OnNa
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
+        playSound.play();
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
