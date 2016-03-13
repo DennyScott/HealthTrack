955e3a6ca7ee007d0793bcc028dc8e7ee12c1ea3
Robby Singh <robby314singh@gmail.com>
2016-03-13 16:43:46 -0500

    Refractored by encapsulating fields

--- app/src/main/java/business/Nutrient.java
+++ app/src/main/java/business/Nutrient.java
@@ -4,8 +4,40 @@ package business;
  * Created by scott on 02/03/16.
  */
 public class Nutrient implements  ApplicationConstants  {
-    int NutrientID;
-    String NutrientSymbol;
-    String NutrientUnit;
-    String NutrientName;
+    private int NutrientID;
+    private String NutrientSymbol;
+    private String NutrientUnit;
+    private String NutrientName;
+
+    public int getNutrientID() {
+        return NutrientID;
+    }
+
+    public void setNutrientID(int nutrientID) {
+        NutrientID = nutrientID;
+    }
+
+    public String getNutrientSymbol() {
+        return NutrientSymbol;
+    }
+
+    public void setNutrientSymbol(String nutrientSymbol) {
+        NutrientSymbol = nutrientSymbol;
+    }
+
+    public String getNutrientUnit() {
+        return NutrientUnit;
+    }
+
+    public void setNutrientUnit(String nutrientUnit) {
+        NutrientUnit = nutrientUnit;
+    }
+
+    public String getNutrientName() {
+        return NutrientName;
+    }
+
+    public void setNutrientName(String nutrientName) {
+        NutrientName = nutrientName;
+    }
 }
