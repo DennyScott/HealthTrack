e839f1866f0a56f602975fa879ff78b9f304497b
Scott Woodcock <umwoodcs@myumanitoba.ca>
2016-03-13 19:23:30 -0500

    Added Tests and Journal Object Database access
    Added unit tests for stats data access class
    Added database query for Journal Objects stores entries in an arraylist Journal Entry objects. Still needs food object constructor to operate.

---
+++ app/src/main/java/business/NotificationDataAccess.java
@@ -0,0 +1,17 @@
+package business;
+
+import java.sql.Date;
+import java.util.ArrayList;
+
+import persistence.DatabaseDefinition;
+
+/**
+ * Created by scott on 11/03/16.
+ */
+public class NotificationDataAccess {
+    public static double getTotalNutrientIntakeByDateRange(String type, ArrayList<Date> dateRange){
+        DatabaseDefinition datadef = new DatabaseDefinition(null,null,null,1,null);
+
+        return 25.5;
+    }
+}
--- app/src/main/java/club/glamajestic/healthtrack/htNotification.java
+++ app/src/main/java/club/glamajestic/healthtrack/htNotification.java
@@ -3,6 +3,10 @@ package club.glamajestic.healthtrack;
 import android.app.NotificationManager;
 import android.content.Context;
 import android.support.v4.app.NotificationCompat;
+
+import java.sql.Date;
+import java.util.ArrayList;
+
 import persistence.*;
 
 /**
@@ -14,20 +18,28 @@ import persistence.*;
 public class htNotification {
 
     private String type;
+    private double amount;
 
-    public htNotification(String type) {
+    public htNotification(String type, double amount) {
         this.type = type;
+        this.amount = amount;
     }
 
-    public static void throwNotification(String type, int amount, Context context) {
-        DatabaseDefinition datadef = new DatabaseDefinition(context,null,null,1,null);
+    /**
+     * Throws the notification in the background to the user.
+     *
+     * @param type The nutritional type whose limit has been exceeded.
+     * @param dateRange The amount that the type has been exceeded by.
+     * @param context Allows access so the notification can be sent.
+     */
+    public void throwNotification(String type, ArrayList<Date> dateRange, Context context) {
         NotificationCompat.Builder notBuilder = new NotificationCompat.Builder(context)
                 .setSmallIcon(android.R.drawable.ic_popup_disk_full)
                 .setContentTitle("HealthTrack")
                 .setContentText("You have exceeded your limit for " + type + " of " + amount);
         NotificationManager mNotificationManager =
                 (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
-
+// mId allows you to update the notification later on.
         mNotificationManager.notify(1, notBuilder.build());
     }
 }
