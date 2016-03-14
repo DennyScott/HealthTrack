ffce78da13814b20da26385de94b4a60655f9a59
Robby Singh <robby314singh@gmail.com>
2016-03-14 00:53:14 -0500

    Renamed test file folder; Brought unit tests together

---
+++ app/src/test/java/club/glamajestic/unittests/KeyValuePairTest.java
@@ -0,0 +1,17 @@
+package business;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by Robby on 3/14/2016.
+ */
+public class KeyValuePairTest extends TestCase {
+
+    public void testEquals() throws Exception {
+
+    }
+
+    public void testCompareTo() throws Exception {
+
+    }
+}
