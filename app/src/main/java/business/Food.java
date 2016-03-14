74895fa26a79c31ce33f696342a9d606a6e5885c
Robby Singh <robby314singh@gmail.com>
2016-03-14 00:07:12 -0500

    Refractored by encapsualtion on Food.java

--- app/src/main/java/business/Food.java
+++ app/src/main/java/business/Food.java
@@ -6,9 +6,49 @@ import java.util.Map;
  * Created by scott on 02/03/16.
  */
 public class Food implements  ApplicationConstants {
-    int FoodID;
-    int FoodGroupID;
-    String FoodDescription;
-    String FoodScientificName;
-    Map<Nutrient, Double> NutrientList;
+    private int FoodID;
+    private int FoodGroupID;
+    private String FoodDescription;
+    private String FoodScientificName;
+    private Map<Nutrient, Double> NutrientList;
+
+    public int getFoodID() {
+        return FoodID;
+    }
+
+    public void setFoodID(int foodID) {
+        FoodID = foodID;
+    }
+
+    public int getFoodGroupID() {
+        return FoodGroupID;
+    }
+
+    public void setFoodGroupID(int foodGroupID) {
+        FoodGroupID = foodGroupID;
+    }
+
+    public String getFoodDescription() {
+        return FoodDescription;
+    }
+
+    public void setFoodDescription(String foodDescription) {
+        FoodDescription = foodDescription;
+    }
+
+    public String getFoodScientificName() {
+        return FoodScientificName;
+    }
+
+    public void setFoodScientificName(String foodScientificName) {
+        FoodScientificName = foodScientificName;
+    }
+
+    public Map<Nutrient, Double> getNutrientList() {
+        return NutrientList;
+    }
+
+    public void setNutrientList(Map<Nutrient, Double> nutrientList) {
+        NutrientList = nutrientList;
+    }
 }
