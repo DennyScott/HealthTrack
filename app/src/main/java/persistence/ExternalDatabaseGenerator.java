a98e6f4ad36ded94104d3cc16f9d14488e47a2ca
Robby Singh <robby314singh@gmail.com>
2016-04-01 12:13:06 -0500

    Updated database with new foods pattern. 300KB->3.2MB
    Added more common foods and the list of where they were retrieved. Should be good enough for now

--- app/src/main/java/persistence/ExternalDatabaseGenerator.java
+++ app/src/main/java/persistence/ExternalDatabaseGenerator.java
@@ -1,11 +1,52 @@
 package persistence;
 
-import android.content.Context;
-import android.database.DatabaseErrorHandler;
-import android.database.sqlite.SQLiteDatabase;
-
 public class ExternalDatabaseGenerator {
 
+    //foods patterns based on top 25 most common foods
+    //http://www.huffingtonpost.ca/2012/05/31/most-popular-food-and-drinks_n_1560021.html\
+    //https://en.wikipedia.org/wiki/Staple_food
+    static final String foodsPattern[] = {
+            "apple",
+            "bacon",
+            "banana",
+            "bean",
+            "beef",
+            "berri",
+            "berry",
+            "broccoli",
+            "carrot",
+            "cereal",
+            "cheese",
+            "chicken",
+            "coffee",
+            "corn",
+            "egg",
+            "fish",
+            "fruit",
+            "garlic",
+            "grape",
+            "mango",
+            "milk",
+            "onion",
+            "orange",
+            "pizza",
+            "pork",
+            "potato",
+            "ribs",
+            "rice",
+            "salad",
+            "soup",
+            "soy",
+            "tea",
+            "wheat",
+            "yam",
+            "yogurt",
+            "bread"
+    };
+    static final String[] filenames = {"extdb/FOOD NAME.csv",
+            "extdb/NUTRIENT AMOUNT.csv",
+            "extdb/CONVERSION FACTOR.csv"
+    };
 
 //    String[] filenames = {
 //            "extdb/FOOD NAME.csv",
@@ -16,15 +57,30 @@ public class ExternalDatabaseGenerator {
 //            "extdb/REFUSE AMOUNT.csv",
 //            "extdb/REFUSE NAME.csv"
 //    };
-
-
-    static final String foodsPattern[] = {
-            "apple",
-            "cheese"
+    static final String idReplacements[] = {
+            "extdb/FOOD GROUP.csv",
+            "extdb/NUTRIENT NAME.csv",
+            "extdb/MEASURE NAME.csv"
     };
-    static final String[] filenames = {"extdb/FOOD NAME.csv",
-            "extdb/NUTRIENT AMOUNT.csv",
-            "extdb/CONVERSION FACTOR.csv"
+    //replacement columns to take off the garbage from the CSV files
+    //  note that listColumns can also do this, but its a lot easier doing it here
+    //  and this will actually remove the columns from the entries (vs listing the ones that are relevant)
+    //  so listColumns does NOT modify the memory but this will
+    static final String columnsToKeep[] = {
+            "FoodDescription",
+            "ScientificName",
+            "NutrientID",   //important for connecting corresponding columns
+            "NutrientValue",
+            "ConversionFactorValue",
+            "FoodGroupName",
+            "NutrientCode",
+            "NutrientSymbol",
+            "NutrientUnit",
+            "NutrientName",
+            "NutrientNameF",
+            "Tagname",      //nutrient tag names will be used as heading names
+            "NutrientDecimals",
+            "MeasureDescription"
     };
 
     //Food name has:
@@ -52,34 +108,6 @@ public class ExternalDatabaseGenerator {
     //Measure Name has
     //  Nothing
     //  Ties with NutrientID from NutAmnt
-
-    static final String idReplacements[] = {
-            "extdb/FOOD GROUP.csv",
-            "extdb/NUTRIENT NAME.csv",
-            "extdb/MEASURE NAME.csv"
-    };
-
-    //replacement columns to take off the garbage from the CSV files
-    //  note that listColumns can also do this, but its a lot easier doing it here
-    //  and this will actually remove the columns from the entries (vs listing the ones that are relevant)
-    //  so listColumns does NOT modify the memory but this will
-    static final String columnsToKeep[] = {
-            "FoodDescription",
-            "ScientificName",
-            "NutrientID",   //important for connecting corresponding columns
-            "NutrientValue",
-            "ConversionFactorValue",
-            "FoodGroupName",
-            "NutrientCode",
-            "NutrientSymbol",
-            "NutrientUnit",
-            "NutrientName",
-            "NutrientNameF",
-            "Tagname",      //nutrient tag names will be used as heading names
-            "NutrientDecimals",
-            "MeasureDescription"
-    };
-
     static final String nutrientsToKeep[] = {
             "ALCOHOL",
             "CAFFEINE",
@@ -125,6 +153,8 @@ public class ExternalDatabaseGenerator {
             "ZINC"
     };
     private static final boolean LIST_FOODS = false;
+    static String timeStartMessage;
+    static long timeStart;
 
     public static void main(String[] args) {
         //testing the converter
@@ -197,10 +227,13 @@ public class ExternalDatabaseGenerator {
         //generate SQL queries for the foods
 
         //create the java class object for it
-        converter.outputJavaObjectToText("DataExternalFoodDb","DataNutrientTable");
-
+        timeStart("Outputting the Java object classes");
+        converter.outputJavaObjectToText("DataExternalFoodDb", "DataNutrientTable");
+        timeEnd();
         //output to SQL
-        converter.createSQLiteDatabase("external_db","ExternalFoods");
+        timeStart("Creating the SQLite database");
+        converter.createSQLiteDatabase("external_db", "ExternalFoods");
+        timeEnd();
     }
 
     private static void timeEnd() {
@@ -208,14 +241,11 @@ public class ExternalDatabaseGenerator {
                 (System.currentTimeMillis() - timeStart));
     }
 
-    static String timeStartMessage;
-    static long timeStart;
     private static void timeStart(String s) {
         timeStartMessage = s;
         timeStart = System.currentTimeMillis();
         System.out.println(s);
     }
-
 
 
 }
